7ea995fa78e43b23eefbb82c13b38f5e6ea76b2e
McVaxius <135515620+McVaxius@users.noreply.github.com>
2026-03-31 14:58:56 -0400

    Stop AutoDuty when MOGTOME stops

--- MOGTOME/Services/MogtomeEngine.cs
+++ MOGTOME/Services/MogtomeEngine.cs
@@ -319,6 +319,7 @@ public class MogtomeEngine
 
         try
         {
+            autoDutyIPC.StopDuty();
             dialogHandler.Stop();
             rotationService.DisableRotation();
             dutyQueue.EnableAutoQueueAfterRepair();
